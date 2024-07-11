88b63e53d3c7b317ccdb2ef0af984a33570a3efe
shadowsnavi <kumori.k.kuro@gmail.com>
2024-07-11 12:14:48 -0700

    complete lab

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,26 @@
 function selectionSort(arr) {
   // type your code here
+  const lowest = {
+    index: -1,
+    value: 0
+  }
+  for (let i = 0; i < arr.length - 1; i++) {
+    lowest.index = i;
+    lowest.value = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      const lowestValue = Math.min(lowest.value, arr[j]);
+      if (lowestValue === arr[j] && lowestValue != lowest.value) {
+        lowest.index = j;
+        lowest.value = lowestValue;
+      }
+    }
+    if (lowest.index !== i) {
+      const temp = arr[i];
+      arr[i] = arr[lowest.index];
+      arr[lowest.index] = temp;
+    }
+  }
+  return arr;
 }
 
 if (require.main === module) {
